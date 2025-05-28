1086132accb803cd3f3144be17a10e3c15952dd5
prem <prem100891@users.noreply.github.com>
2025-05-28 20:15:10 +0530

    updated code

---
+++ src/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h3>Welcome to Chat App 🎉</h3>
+      {/* List of registered users and groups will go here */}
+    </div>
+  );
+}
+
+export default Dashboard;
