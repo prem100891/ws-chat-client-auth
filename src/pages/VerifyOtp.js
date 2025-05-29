72ee5006d9e98993ad533601ddd7076d7cefa15a
prem <prem100891@users.noreply.github.com>
2025-05-29 18:42:03 +0530

    complete ui code

--- src/pages/VerifyOtp.js
+++ src/pages/VerifyOtp.js
@@ -37,8 +37,9 @@ const VerifyOtp = () => {
         headers: { 'Content-Type': 'application/json' }
       });
   
-      console.log("res-->",res)
+      console.log("res-->",res);
       if (res.status === 200 || res.data.success == true || res.data.message == "OTP Verified") {
+        console.log(res.data, 'res.data--->>')
         localStorage.setItem("user", JSON.stringify({ phone, name }));
         setSnack({ open: true, message: "✅ OTP Verified", type: "success" });
         navigate("/dashboard");
